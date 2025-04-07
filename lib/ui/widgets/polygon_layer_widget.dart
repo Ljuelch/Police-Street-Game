45c978b6ee857688d2e33b44fb285b3b0ff5d457
juelchl <leonardo.juelch@berlintransport.de>
2025-04-07 18:31:23 +0200

    new: win/los menu

--- lib/ui/widgets/polygon_layer_widget.dart
+++ lib/ui/widgets/polygon_layer_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter/material.dart';
-import 'package:latlong2/latlong.dart';
 import '../../sector_polygon.dart';
 
 class PolygonLayerWidget extends StatelessWidget {
